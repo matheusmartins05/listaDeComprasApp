0ae7e8b3f17a0614db0c8e8986fd37514746ac52
matheusmartins05 <matheusmartins2901@gmail.com>
2024-06-17 08:54:29 -0300

    Adicionada scroll à seção de itens

--- src/components/SectionItens.tsx
+++ src/components/SectionItens.tsx
@@ -6,7 +6,7 @@ export default function SectionItens() {
   const [listaDosItensInseridos, setListaDosItensInseridos] = useRecoilState(listaDeItens);
 
   return (
-    <section className="bg-[#0c0c0d] h-screen text-gray-400 w-[100%] py-10 flex flex-col gap-3">
+    <section id="scroll" className="bg-[#0c0c0d] h-80 overflow-auto text-gray-400 w-[100%] py-10 flex flex-col gap-3">
       {listaDosItensInseridos.map((item) => (
         <CardItem 
             key={item.id} 
